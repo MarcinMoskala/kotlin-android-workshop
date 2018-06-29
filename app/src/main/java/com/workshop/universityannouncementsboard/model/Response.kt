2adfc5fe3609168e048c464d5435240deb63f0fe
Marcin Moskala <marcinmoskala@gmail.com>
2018-06-28 19:53:27 -0700

    Update for response requirements

--- app/src/main/java/com/workshop/universityannouncementsboard/model/Response.kt
+++ app/src/main/java/com/workshop/universityannouncementsboard/model/Response.kt
@@ -3,7 +3,7 @@ package com.workshop.universityannouncementsboard.model
 /* TODO:
    I would like to use it this way:
 
-   fun main(args: Array<String>) {
+fun main(args: Array<String>) {
     val rs1 = Success(1)
     val re1 = ErrorResponse(Error())
     val re2 = ErrorResponse("Error")
@@ -13,6 +13,17 @@ package com.workshop.universityannouncementsboard.model
 
     val r3: Response<Int, String> = rs1
     val r4: Response<Int, String> = re2
+
+    val r5: Response<Any, Throwable> = rs1
+    val r6: Response<Any, Throwable> = re1
+
+    val s = Success(String())
+    val s1: Success<CharSequence> = s
+    val s2: Success<Any> = s
+
+    val e = ErrorResponse(Error())
+    val e1: ErrorResponse<Throwable> = e
+    val e2: ErrorResponse<Any> = e
 }
  */
 sealed class Response<R, E>
