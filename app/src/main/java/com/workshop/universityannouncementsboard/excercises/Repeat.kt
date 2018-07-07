80df206bc13ef14eac40c4badd2471fc114041e0
Marcin Moskala <marcinmoskala@gmail.com>
2018-07-07 12:27:25 +0100

    Move exercises to separate project

--- app/src/main/java/com/workshop/universityannouncementsboard/excercises/Repeat.kt
+++
@@ -1,9 +0,0 @@
-package com.workshop.universityannouncementsboard.excercises
-
-// TODO: Write your own repeat function:
-
-private fun repeat(times: Int, action: () -> Unit) {}
-
-fun main(args: Array<String>) {
-    repeat(5) { print("A") } // AAAAA
-}
