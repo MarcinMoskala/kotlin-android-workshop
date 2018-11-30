5e7367191c62bbe4955127a01b329b40173b2d27
Marcin Moskala <marcinmoskala@gmail.com>
2018-11-30 21:30:13 +0100

    plusAt should be public

--- app/src/main/java/com/workshop/universityannouncementsboard/extra/PlusAt.kt
+++ app/src/main/java/com/workshop/universityannouncementsboard/extra/PlusAt.kt
@@ -2,6 +2,6 @@ package com.workshop.universityannouncementsboard.extra
 
 // TODO Implement this function to return list with element added at `index` position.
 // Check out unit tests at PlusAtTest class.
-private fun <T> List<T>.plusAt(index: Int, element: T): List<T> {
+fun <T> List<T>.plusAt(index: Int, element: T): List<T> {
     TODO()
 }
