6037f40153f53bcf2d06d54a51d972fcd9c61815
Marcin Moskala <marcinmoskala@gmail.com>
2018-12-12 12:31:45 +0100

    Add default delegates for MainActivity swipeRefresh and loading properties

--- app/src/main/java/com/workshop/universityannouncementsboard/presentation/MainActivity.kt
+++ app/src/main/java/com/workshop/universityannouncementsboard/presentation/MainActivity.kt
@@ -2,19 +2,26 @@ package com.workshop.universityannouncementsboard.presentation
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.workshop.universityannouncementsboard.R
 import com.workshop.universityannouncementsboard.model.Announcement
 import com.workshop.universityannouncementsboard.repositiories.AnnouncementsRepositoryImpl
 import com.workshop.universityannouncementsboard.repositiories.StudentsRepositoryImpl
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity(), MainView {
 
-    // TODO: Write property delegate that will bind it to visibility of R.id.progressView
+    // TODO: Write property delegate that will bind it to visibility of R.id.progressView. Usage should look this way
     // override var loading: Boolean by bindToVisibility(R.id.progressView)
-    override var loading: Boolean = false
-    // TODO: Write property delegate that will bind it to swipe refresh on R.id.swipeRefreshView
+    override var loading: Boolean by Delegates.observable(false) { _, _, newValue ->
+        progressView.visibility = if(newValue) View.VISIBLE else View.GONE
+    }
+    // TODO: Write property delegate that will bind it to swipe refresh on R.id.swipeRefreshView. Usage should look this way
     // override var swipeRefresh: Boolean by bindToSwipeRefresh(R.id.swipeRefreshView)
-    override var swipeRefresh: Boolean = false
+    override var swipeRefresh: Boolean by Delegates.observable(false) { _, _, newValue ->
+        swipeRefreshView.isRefreshing = newValue
+    }
 
     private val studentsRepository by lazy { StudentsRepositoryImpl() }
     private val announcementsRepository by lazy { AnnouncementsRepositoryImpl(studentsRepository) }
