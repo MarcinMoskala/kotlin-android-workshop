9a0f4f4c552c353a0af4dfb1fcaad0a4019295f5
Marcin Moskala <marcinmoskala@gmail.com>
2019-02-19 09:30:30 +0000

    Add task `rational`

---
+++ app/src/main/java/com/workshop/universityannouncementsboard/extra/Rational.kt
@@ -0,0 +1,6 @@
+package com.workshop.universityannouncementsboard.extra
+
+data class RationalNumber(val numerator: Int, val denominator: Int)
+
+fun Int.r(): RationalNumber = RationalNumber(this, 1)
+fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(first, second)
