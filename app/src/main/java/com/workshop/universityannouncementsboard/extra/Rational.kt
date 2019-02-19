aee0ed3027a85654e0c26a824942aacb610a53c8
Marcin Moskala <marcinmoskala@gmail.com>
2019-02-19 09:31:49 +0000

    Fix task `rational`

--- app/src/main/java/com/workshop/universityannouncementsboard/extra/Rational.kt
+++ app/src/main/java/com/workshop/universityannouncementsboard/extra/Rational.kt
@@ -2,5 +2,5 @@ package com.workshop.universityannouncementsboard.extra
 
 data class RationalNumber(val numerator: Int, val denominator: Int)
 
-fun Int.r(): RationalNumber = RationalNumber(this, 1)
-fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(first, second)
+fun Int.r(): RationalNumber = TODO()
+fun Pair<Int, Int>.r(): RationalNumber = TODO()
