d3ba1f2566c9e0326fb696b992f50d21c8d0e8dc
Marcin Moskala <marcinmoskala@gmail.com>
2019-05-09 11:01:52 +0100

    Add comments explaining expectations to collection processing tasks

--- app/src/main/java/com/workshop/universityannouncementsboard/repositiories/AnnouncementsRepositoryImpl.kt
+++ app/src/main/java/com/workshop/universityannouncementsboard/repositiories/AnnouncementsRepositoryImpl.kt
@@ -1,28 +1,35 @@
 package com.workshop.universityannouncementsboard.repositiories
 
-import com.workshop.universityannouncementsboard.AnnouncementsList
-import com.workshop.universityannouncementsboard.model.Announcement
-import com.workshop.universityannouncementsboard.model.ErrorResponse
-import com.workshop.universityannouncementsboard.model.Response
-import com.workshop.universityannouncementsboard.model.Success
-import java.util.*
+import com.workshop.universityannouncementsboard.*
+import com.workshop.universityannouncementsboard.model.*
+import kotlin.random.*
 
 class AnnouncementsRepositoryImpl(val studentsRepository: StudentsRepository) : AnnouncementsRepository {
 
     override fun getAnnouncements(): Response<List<Announcement>, Throwable> {
         Thread.sleep(2000) // Nobody trust university system that works too fast
-        if (Random().nextBoolean()) return ErrorResponse(Error("Random error")) // Nobody trust university system that is fully reliable
+        if (Random.nextBoolean()) return ErrorResponse(Error("Random error")) // Nobody trust university system that is fully reliable
         val announcements = AnnouncementsList.getAnnouncements(
-                passingStudentsListText = makePassingStudentsListText(),
-                bestStudentsListText = makePassingStudentsListText()
+            passingStudentsListText = makePassingStudentsListText(),
+            bestStudentsListText = makePassingStudentsListText()
         )
         return Success(announcements)
     }
 
+    /*
+     * Displays a list of students who have got more than 15 points in the semester and a result of
+     * at least 50, in alphabetical order (surname then name), in the format: “{name} {surname}, {result}”
+     */
     // TODO: Should return passing students list. See PassingStudentsListTest
     fun makePassingStudentsListText(): String = "" // Get students suing studentsRepository.getStudents()
 
-
+    /*
+     * Displays the best 10 students so they can get an internship. Comparing students by result
+     * (higher is better). To get an internship, students need to have got at least 30 points
+     * in the semester and a result of at least 80. The best student gets $5000, the next 3 get
+     * $3000 and the next 6 get $1000. Display students in alphabetical order (compare first surname
+     * then name) in the format “{name} {surname}, ${internship size}”
+     */
     // TODO: Should return students for internship. See BestStudentsListTest
     fun makeBestStudentsList(): String = "" // Get students suing studentsRepository.getStudents()
 }
