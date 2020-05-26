270a2c9877ca6021327183d5edccea47d7cce192
Marcin Moskala <marcinmoskala@gmail.com>
2020-05-26 13:16:32 +0200

    Bump Gradle version, Rename "ErrorResponse" -> "Failure"

--- app/src/main/java/com/workshop/universityannouncementsboard/model/Response.kt
+++ app/src/main/java/com/workshop/universityannouncementsboard/model/Response.kt
@@ -12,19 +12,19 @@ fun processResponseString(response: Response<String, Throwable>) { /*...*/ }
 
 fun usage() {
     processResponseInt(Success(1))
-    processResponseInt(ErrorResponse("ERROR"))
+    processResponseInt(Failure("ERROR"))
     processResponseString(Success("ERROR"))
-    processResponseString(ErrorResponse(Error("ERROR")))
+    processResponseString(Failure(Error("ERROR")))
 
     val rs1 = Success(1)
-    val re1 = ErrorResponse(Error())
-    val re2 = ErrorResponse("Error")
+    val re1 = Failure(Error())
+    val re2 = Failure("Error")
 
     val rs1asNumber: Success<Number> = rs1
     val rs1asAny: Success<Any> = rs1
 
-    val re1asThrowable: ErrorResponse<Throwable> = re1
-    val re1asAny: ErrorResponse<Any> = re1
+    val re1asThrowable: Failure<Throwable> = re1
+    val re1asAny: Failure<Any> = re1
 
     val r1: Response<Int, Error> = rs1
     val r2: Response<Int, Error> = re1
@@ -39,8 +39,8 @@ fun usage() {
     val s1: Success<CharSequence> = s
     val s2: Success<Any> = s
 
-    val e = ErrorResponse(Error())
-    val e1: ErrorResponse<Throwable> = e
-    val e2: ErrorResponse<Any> = e
+    val e = Failure(Error())
+    val e1: Failure<Throwable> = e
+    val e2: Failure<Any> = e
 }
 */
