aeb83013af0dd41d73f18e9f2b7c8d65059a2b9f
Marcin Moskala <marcin.moskala@allegro.pl>
2020-10-30 10:14:26 +0100

    Cleanup Base Presenter

--- app/src/main/java/com/workshop/universityannouncementsboard/domain/BasePresenter.kt
+++ app/src/main/java/com/workshop/universityannouncementsboard/domain/BasePresenter.kt
@@ -5,11 +5,8 @@ import kotlin.coroutines.CoroutineContext
 
 abstract class BasePresenter(
         private val onError: (Throwable) -> Unit = {}
-): CoroutineScope {
-    private val handler = CoroutineExceptionHandler { _, throwable -> onError(throwable) }
-    override val coroutineContext: CoroutineContext = Dispatchers.Main + SupervisorJob() + handler
+) {
 
     fun onDestroy() {
-        coroutineContext.cancelChildren()
     }
 }
